5fede333061375af42be5fae452edeced6258415
Thomas Sachau <tommy@gentoo.org>
2022-03-21 11:31:54 +0100

    Fix intend
    Signed-off-by: Thomas Sachau <tommy@gentoo.org>

--- lib/_emerge/actions.py
+++ lib/_emerge/actions.py
@@ -3417,13 +3417,13 @@ def run_action(emerge_config):
 
     apply_priorities(emerge_config.target_config.settings)
 
-	if 'force-multilib' in emerge_config.target_config.settings.features:
-		if emerge_config.target_config.settings.get("NO_AUTO_FLAG", "") == "":
-			writemsg_level(bad("!!! Failed to find vars from extra profile") + "\n",level=logging.ERROR, noiselevel=-1)
-			writemsg_level(bad("!!! Please make sure that you did follow the instructions and included the extra profile\n"),level=logging.ERROR, noiselevel=-1)
-			writemsg_level(bad("!!! http://git.overlays.gentoo.org/gitweb/?p=proj/multilib-portage.git;a=blob;f=doc/portage-multilib-instructions\n"),level=logging.ERROR, noiselevel=-1)
-			writemsg_level(bad("!!! has some basic instructions for the setup\n"),level=logging.ERROR, noiselevel=-1)
-			return 1
+    if 'force-multilib' in emerge_config.target_config.settings.features:
+        if emerge_config.target_config.settings.get("NO_AUTO_FLAG", "") == "":
+            writemsg_level(bad("!!! Failed to find vars from extra profile") + "\n",level=logging.ERROR, noiselevel=-1)
+            writemsg_level(bad("!!! Please make sure that you did follow the instructions and included the extra profile\n"),level=logging.ERROR, noiselevel=-1)
+            writemsg_level(bad("!!! http://git.overlays.gentoo.org/gitweb/?p=proj/multilib-portage.git;a=blob;f=doc/portage-multilib-instructions\n"),level=logging.ERROR, noiselevel=-1)
+            writemsg_level(bad("!!! has some basic instructions for the setup\n"),level=logging.ERROR, noiselevel=-1)
+            return 1
     if (
         "--autounmask-continue" in emerge_config.opts
         and emerge_config.opts.get("--autounmask") == "n"
